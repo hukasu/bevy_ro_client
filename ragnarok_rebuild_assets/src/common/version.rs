71ee8fff37342612281da6742fce51ad53134695
Lucas Farias <lucasfnf@gmail.com>
2024-09-23 22:14:20 -0300

    Add new method to Version

--- ragnarok_rebuild_assets/src/common/version.rs
+++ ragnarok_rebuild_assets/src/common/version.rs
@@ -8,6 +8,22 @@ use crate::{gnd, grf, rsm, rsw};
 pub struct Version(pub u8, pub u8, pub u32);
 
 impl Version {
+    pub fn new(major: u8, minor: u8, patch: u32) -> Self {
+        Self(major, minor, patch)
+    }
+
+    pub fn major(&self) -> u8 {
+        self.0
+    }
+
+    pub fn minor(&self) -> u8 {
+        self.1
+    }
+
+    pub fn patch(&self) -> u32 {
+        self.2
+    }
+
     pub fn gnd_version_from_reader(mut reader: &mut dyn Read) -> Result<Self, gnd::Error> {
         let major = reader.read_u8()?;
         let minor = reader.read_u8()?;
