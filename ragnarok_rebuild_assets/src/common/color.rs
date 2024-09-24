b12d7aa41dc47d818d5cb0b2762ee36004a909a0
Lucas Farias <lucasfnf@gmail.com>
2024-09-24 09:45:54 -0300

    Impl Display for Color

--- ragnarok_rebuild_assets/src/common/color.rs
+++ ragnarok_rebuild_assets/src/common/color.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Color<T> {
     pub red: T,
@@ -17,5 +19,15 @@ impl From<UColor> for FColor {
             blue: value.blue as f32 / 255.,
             alpha: value.alpha as f32 / 255.,
         }
+    }
+}
+
+impl Display for UColor {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Color #{:02x}{:02x}{:02x}{:02x}",
+            self.red, self.green, self.blue, self.alpha
+        )
     }
 }
