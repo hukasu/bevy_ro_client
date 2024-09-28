1ee55d31f0ea1347250233b5fc6aac656f019755
Lucas Farias <lucasfnf@gmail.com>
2024-09-28 10:01:04 -0300

    Add check for sprite layer using negative spritesheet id (other than -1)

--- ragnarok_rebuild_assets/src/bin/act_debug.rs
+++ ragnarok_rebuild_assets/src/bin/act_debug.rs
@@ -1,7 +1,7 @@
 use std::{fmt::Write, io::Cursor, path::Path};
 
 use ragnarok_rebuild_assets::{
-    act::{Act, AnimationClip, AnimationFrame},
+    act::{Act, AnimationClip, AnimationFrame, SpriteLayer},
     grf::GRF,
 };
 
@@ -21,12 +21,12 @@ fn main() {
     {
         let Ok(act_content) = grf
             .read_file(act_filename)
-            .inspect_err(|err| eprintln!("{act_filename:?}: {err}"))
+            .inspect_err(|err| println!("{act_filename:?}: {err}"))
         else {
             continue;
         };
         let Ok(act) = Act::from_reader(&mut Cursor::new(act_content))
-            .inspect_err(|err| eprintln!("{act_filename:?}: {err}"))
+            .inspect_err(|err| println!("{act_filename:?}: {err}"))
         else {
             continue;
         };
@@ -72,7 +72,8 @@ fn debug_animation_clip(clip: &AnimationClip, clip_index: usize, act: &Act) -> O
 }
 
 fn debug_animation_frame(frame: &AnimationFrame, frame_index: usize, act: &Act) -> Option<String> {
-    let header = || format!("\t\t\tFrame {:?}\n", frame_index);
+    const INDENT: &str = "\t\t\t";
+    let header = || format!("{INDENT}Frame {:?}\n", frame_index);
     let mut debug = None;
 
     if frame.animation_event_id != -1
@@ -81,10 +82,48 @@ fn debug_animation_frame(frame: &AnimationFrame, frame_index: usize, act: &Act) 
         let debug_ref = debug.get_or_insert_with(header);
         writeln!(
             debug_ref,
-            "\t\t\tFrame {} has event outside of bounds. Act has {} events but frame event id was {}.",
+            "{INDENT}\tFrame {} has event outside of bounds. Act has {} events but frame event id was {}.",
             frame_index,
             act.animation_events.len(),
             frame.animation_event_id
+        )
+        .unwrap();
+    }
+
+    for (i, layer) in frame.sprite_layers.iter().enumerate() {
+        if let Some(animation_frame_debug) = debug_animation_layer(layer, i) {
+            let debug_ref = debug.get_or_insert_with(header);
+            write!(debug_ref, "{}", animation_frame_debug).unwrap();
+        }
+    }
+
+    debug
+}
+
+fn debug_animation_layer(layer: &SpriteLayer, frame_index: usize) -> Option<String> {
+    const INDENT: &str = "\t\t\t\t";
+    let header = || format!("{INDENT}Layer {:?}\n", frame_index);
+    let mut debug = None;
+
+    match layer.image_type_id {
+        0 | 1 => (),
+        id => {
+            let debug_ref = debug.get_or_insert_with(header);
+            writeln!(
+                debug_ref,
+                "{INDENT}\tLayer has unknown image type id ({}).",
+                id
+            )
+            .unwrap();
+        }
+    }
+
+    if layer.spritesheet_cell_index < -1 {
+        let debug_ref = debug.get_or_insert_with(header);
+        writeln!(
+            debug_ref,
+            "{INDENT}\tLayer has negative spritesheet index ({}).",
+            layer.spritesheet_cell_index
         )
         .unwrap();
     }
