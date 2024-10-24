25c98fbd3cc28a7332f669e4f76e205e7cbd110d
Lucas Farias <lucasfnf@gmail.com>
2024-10-24 14:06:59 -0300

    Rename members of `Imf` structs

--- ragnarok_rebuild_assets/src/imf/animation.rs
+++ ragnarok_rebuild_assets/src/imf/animation.rs
@@ -4,17 +4,17 @@ use ragnarok_rebuild_common::reader_ext::ReaderExt;
 
 #[derive(Debug)]
 pub struct Animation {
-    pub frames_data: Box<[super::Frame]>,
+    pub frames: Box<[super::Frame]>,
 }
 
 impl Animation {
     pub fn from_reader(mut reader: &mut dyn Read) -> Result<Self, super::Error> {
-        let frame_data_count = reader.read_le_u32()?;
+        let frame_count = reader.read_le_u32()?;
 
-        let frames_data = (0..frame_data_count)
+        let frames = (0..frame_count)
             .map(|_| super::Frame::from_reader(reader))
             .collect::<Result<_, _>>()?;
 
-        Ok(Self { frames_data })
+        Ok(Self { frames })
     }
 }
--- ragnarok_rebuild_assets/src/imf/layer.rs
+++ ragnarok_rebuild_assets/src/imf/layer.rs
@@ -4,17 +4,17 @@ use ragnarok_rebuild_common::reader_ext::ReaderExt;
 
 #[derive(Debug)]
 pub struct Layer {
-    pub frames: Box<[super::Animation]>,
+    pub animations: Box<[super::Animation]>,
 }
 
 impl Layer {
     pub fn from_reader(mut reader: &mut dyn Read) -> Result<Self, super::Error> {
-        let frame_count = reader.read_le_u32()?;
+        let animation_count = reader.read_le_u32()?;
 
-        let frames = (0..frame_count)
+        let animations = (0..animation_count)
             .map(|_| super::Animation::from_reader(reader))
             .collect::<Result<_, _>>()?;
 
-        Ok(Self { frames })
+        Ok(Self { animations })
     }
 }
