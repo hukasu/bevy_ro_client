60e06e07a6c4a315c3176ccd1967f850700ac9f3
Lucas Farias <lucasfnf@gmail.com>
2025-05-07 12:03:26 -0300

    Start rewrite of RSM debugger

---
+++ ragnarok_debugger/src/bin/rsm_debug.rs
@@ -0,0 +1,35 @@
+use std::{io::Cursor, path::Path};
+
+use ragnarok_rebuild_assets::grf::Grf;
+use ragnarok_rsm::Rsm;
+
+fn main() {
+    #[expect(clippy::unwrap_used, reason = "This is a test")]
+    let grf = Grf::new(Path::new("data.grf")).unwrap();
+
+    for rsm_filename in grf
+        .iter_filenames()
+        .filter(|filename| match filename.extension() {
+            Some(ext) => {
+                matches!(ext.to_str(), Some("rsm") | Some("rsm2"))
+            }
+            None => false,
+        })
+    {
+        let Ok(rsm_content) = grf
+            .read_file(rsm_filename)
+            .inspect_err(|err| println!("{rsm_filename:?}: {err}"))
+        else {
+            continue;
+        };
+        let Ok(rsm) = Rsm::from_reader(&mut Cursor::new(rsm_content))
+            .inspect_err(|err| println!("{rsm_filename:?}: {err}"))
+        else {
+            continue;
+        };
+
+        if !rsm.warnings.is_empty() {
+            println!("{:?} {} {:#?}", rsm_filename, rsm.version, rsm.warnings);
+        }
+    }
+}
