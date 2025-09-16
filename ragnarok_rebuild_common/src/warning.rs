465e802fb181d0df33cf531dd91b96ef1428ffc5
Lucas Farias <lucasfnf@gmail.com>
2025-09-15 21:49:23 -0300

    Typos

--- ragnarok_rebuild_common/src/warning.rs
+++ ragnarok_rebuild_common/src/warning.rs
@@ -2,7 +2,7 @@ use std::fmt::Display;
 
 /// Report warnings that might happen on assets.
 ///
-/// Warnings refer to thigns that are not implemented currrently,
+/// Warnings refer to things that are not implemented currently,
 /// things that require deeper consideration on implementation,
 /// or are plain weird values that don't work.
 pub trait ReportWarning {
