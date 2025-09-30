b77a0540001f8887b2f8548c1b3e2c9291715493
Lucas Farias <lucasfnf@gmail.com>
2025-09-29 22:10:17 -0300

    Add system to invert Rsm material to `bevy_ragnarok_rsm`

--- bevy_crates/bevy_ragnarok_rsm/src/plugin/mod.rs
+++ bevy_crates/bevy_ragnarok_rsm/src/plugin/mod.rs
@@ -1,6 +1,16 @@
 mod loader;
 
+use bevy_app::PostUpdate;
 use bevy_asset::AssetApp;
+use bevy_ecs::{
+    name::NameOrEntity,
+    query::{With, Without},
+    schedule::IntoScheduleConfigs,
+    system::{Commands, Populated},
+};
+use bevy_mesh::Mesh3d;
+use bevy_pbr::MeshMaterial3d;
+use bevy_transform::{TransformSystems, components::GlobalTransform};
 use loader::AssetLoader;
 
 use crate::{Model, RsmMaterials, assets::RsmModel, materials::RsmMaterial};
@@ -19,11 +29,39 @@ impl bevy_app::Plugin for Plugin {
         // Materials
         app.add_plugins(crate::materials::Plugin);
 
+        // Systems
+        app.add_systems(
+            PostUpdate,
+            invert_material.after(TransformSystems::Propagate),
+        );
+
         // Types
         app.register_type::<Model>();
         app.register_type::<RsmMaterials>();
 
         #[cfg(feature = "debug")]
         app.add_plugins(crate::debug::Plugin);
+    }
+}
+
+/// Insert material for models based on the number of negative scale axis.
+/// Even number of negative scale axis uses [`RsmMaterials::base`]. Odd uses
+/// [`RsmMaterials::inverted`].
+#[expect(clippy::type_complexity, reason = "Queries are complex")]
+fn invert_material(
+    mut commands: Commands,
+    rsms: Populated<
+        (NameOrEntity, &RsmMaterials, &GlobalTransform),
+        (With<Mesh3d>, Without<MeshMaterial3d<RsmMaterial>>),
+    >,
+) {
+    for (rsm, rsm_materials, global_transform) in rsms.into_inner() {
+        let inverted_axis = global_transform.scale().is_negative_bitmask().count_ones();
+        let material = if inverted_axis % 2 == 0 {
+            rsm_materials.base.clone()
+        } else {
+            rsm_materials.inverted.clone()
+        };
+        commands.entity(rsm.entity).insert(MeshMaterial3d(material));
     }
 }
