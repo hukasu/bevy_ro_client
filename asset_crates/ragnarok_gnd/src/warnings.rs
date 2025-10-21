c10085d5d5edb46b8fe7b90c1d1c6d87043460aa
Lucas Farias <lucasfnf@gmail.com>
2025-10-21 13:58:20 -0300

    Test for Gnd with faces on the edges

--- asset_crates/ragnarok_gnd/src/warnings.rs
+++ asset_crates/ragnarok_gnd/src/warnings.rs
@@ -24,6 +24,8 @@ impl Display for GndWarning<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.report_dimensions(f)?;
         self.report_surfaces(f)?;
+        self.report_east_edge(f)?;
+        self.report_north_edge(f)?;
         Ok(())
     }
 }
@@ -43,6 +45,42 @@ impl GndWarning<'_> {
         for (i, surface) in self.surfaces.iter().enumerate() {
             if usize::from(surface.texture_id) >= self.textures.len() {
                 writeln!(f, "Surface {i} has invalid texture. {}", surface.texture_id)?;
+            }
+        }
+        Ok(())
+    }
+
+    fn report_east_edge(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let x = self.width - 1;
+        for z in 0..self.height {
+            let Ok(index) = usize::try_from(x + z * self.width) else {
+                unreachable!("Index must fit in usize.");
+            };
+            let cube = &self.ground_mesh_cubes[index];
+            if cube.east_facing_surface != -1 {
+                writeln!(
+                    f,
+                    "Cube {x}/{z} has east face. ({})",
+                    cube.east_facing_surface
+                )?;
+            }
+        }
+        Ok(())
+    }
+
+    fn report_north_edge(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let z = self.height - 1;
+        for x in 0..self.width {
+            let Ok(index) = usize::try_from(x + z * self.width) else {
+                unreachable!("Index must fit in usize.");
+            };
+            let cube = &self.ground_mesh_cubes[index];
+            if cube.north_facing_surface != -1 {
+                writeln!(
+                    f,
+                    "Cube {x}/{z} has north face. ({})",
+                    cube.north_facing_surface
+                )?;
             }
         }
         Ok(())
