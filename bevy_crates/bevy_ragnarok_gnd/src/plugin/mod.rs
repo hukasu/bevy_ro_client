06323efe19c2a9d9afbdc084c1bbbdfcd8e47831
Lucas Farias <lucasfnf@gmail.com>
2025-10-26 19:42:34 -0300

    Fix order of triangles on Gnd quads

--- bevy_crates/bevy_ragnarok_gnd/src/plugin/mod.rs
+++ bevy_crates/bevy_ragnarok_gnd/src/plugin/mod.rs
@@ -15,6 +15,7 @@ use crate::{Cube, Ground, assets::GndAsset, material, plugin::loader::AssetLoade
 const GND_TOP_MESH: Handle<Mesh> = uuid_handle!("886618db-d316-482e-8aeb-c79a73e47f44");
 const GND_EAST_MESH: Handle<Mesh> = uuid_handle!("8ddb2470-39cd-4083-b37d-93d2a84bb2d6");
 const GND_NORTH_MESH: Handle<Mesh> = uuid_handle!("f1bcac29-8498-4f61-8f7c-b07b8112a61b");
+const INDICES: [u16; 6] = [0, 1, 2, 1, 3, 2];
 
 pub struct Plugin {
     pub texture_prefix: Cow<'static, str>,
@@ -63,7 +64,7 @@ impl bevy_app::Plugin for Plugin {
             )
             .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, vec![Vec3::NEG_Y; 4])
             .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, vec![Vec2::ZERO; 4])
-            .with_inserted_indices(Indices::U16(vec![0, 1, 3, 0, 3, 2])),
+            .with_inserted_indices(Indices::U16(INDICES.to_vec())),
         ) {
             error!("{err}");
             app.world_mut().write_message(AppExit::from_code(1));
@@ -87,7 +88,7 @@ impl bevy_app::Plugin for Plugin {
             )
             .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, vec![Vec3::X; 4])
             .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, vec![Vec2::ZERO; 4])
-            .with_inserted_indices(Indices::U16(vec![0, 1, 3, 0, 3, 2])),
+            .with_inserted_indices(Indices::U16(INDICES.to_vec())),
         ) {
             error!("{err}");
             app.world_mut().write_message(AppExit::from_code(1));
@@ -111,7 +112,7 @@ impl bevy_app::Plugin for Plugin {
             )
             .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, vec![Vec3::NEG_Z; 4])
             .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, vec![Vec2::ZERO; 4])
-            .with_inserted_indices(Indices::U16(vec![0, 1, 3, 0, 3, 2])),
+            .with_inserted_indices(Indices::U16(INDICES.to_vec())),
         ) {
             error!("{err}");
             app.world_mut().write_message(AppExit::from_code(1));
