c2d4d83c23bd1c8f9dad77154d17f86395449a59
Lucas Farias <lucasfnf@gmail.com>
2025-10-30 14:05:36 -0300

    Create new version of `GameStates`

--- ragnarok_rebuild_client/src/client/mod.rs
+++ ragnarok_rebuild_client/src/client/mod.rs
@@ -7,7 +7,28 @@ mod loading_screen;
 pub mod states;
 pub mod world;
 
-use bevy::prelude::Component;
+use bevy::{prelude::Component, state::state::ComputedStates};
+
+use crate::client::world::MapChangeStates;
 
 #[derive(Debug, Component)]
 pub struct Game;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum GameStates {
+    Login,
+    MapChange,
+    Game,
+}
+
+impl ComputedStates for GameStates {
+    type SourceStates = MapChangeStates;
+
+    fn compute(sources: Self::SourceStates) -> Option<Self> {
+        match sources {
+            MapChangeStates::Unloaded => Some(Self::Login),
+            MapChangeStates::Loaded => Some(Self::Game),
+            _ => Some(Self::MapChange),
+        }
+    }
+}
